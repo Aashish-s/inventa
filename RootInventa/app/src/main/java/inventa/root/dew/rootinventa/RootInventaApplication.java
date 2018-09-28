83752eea3f21539aafa0f8d6cc127ca1ba4692c1
Arnab <asaha@cuttingchaitech.com>
2018-09-28 12:57:45 +0530

    gradle updates

--- RootInventa/app/src/main/java/inventa/root/dew/rootinventa/RootInventaApplication.java
+++ RootInventa/app/src/main/java/inventa/root/dew/rootinventa/RootInventaApplication.java
@@ -1,14 +1,19 @@
 package inventa.root.dew.rootinventa;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
+import com.google.firebase.FirebaseApp;
 
 import inventa.root.dew.rootinventa.receiver.DiscoveryBroadcastReceiver;
 import proto.inventa.cct.com.inventalibrary.InventaSdk;
 import proto.inventa.cct.com.inventalibrary.discovery.DiscoveryHelper;
 
-public class RootInventaApplication extends Application {
+public class RootInventaApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
+        FirebaseApp.initializeApp(getApplicationContext());
+        super.onCreate();
         InventaSdk.sdkInitialize(getApplicationContext());
         DiscoveryHelper.setDiscoveryTransitionsReceiver(new DiscoveryBroadcastReceiver());
     }
