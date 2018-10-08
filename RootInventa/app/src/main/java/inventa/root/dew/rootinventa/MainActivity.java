ab389c42beeb64aa8947c68be3c41992b8e7fde3
akslive <aashishkrsinha@gmail.com>
2018-10-08 17:18:26 +0530

    adding glide error

--- RootInventa/app/src/main/java/inventa/root/dew/rootinventa/MainActivity.java
+++ RootInventa/app/src/main/java/inventa/root/dew/rootinventa/MainActivity.java
@@ -6,6 +6,8 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.LazyHeaders;
 import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends Activity{
@@ -33,5 +35,24 @@ public class MainActivity extends Activity{
                         .centerCrop()
                         .dontTransform())
                 .into(imgView);
+        // Code using glide request
+        loadImageWithGlideRequests(url, true);
+    }
+
+    private void loadImageWithGlideRequests(String url, boolean b){
+        try {
+            LazyHeaders auth = new LazyHeaders.Builder()
+                    .build();
+            GlideRequests glideRequests = GlideApp.with(this);
+            RequestOptions requestOptions =
+                    new RequestOptions().placeholder(R.drawable.avatar)
+                            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).dontTransform();
+            glideRequests
+                    .load(b ? new GlideUrl(url, auth) : new GlideUrl(url))
+                    .apply(requestOptions)
+                    .into(imgView).clearOnDetach();
+        } catch (Exception e) {
+            // do handling here
+        }
     }
 }
